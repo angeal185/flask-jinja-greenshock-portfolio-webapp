fee110f4a0cf9851028e370b310471204536eba3
Angeal185 <beneaves01@hotmail.com>
2017-02-10 19:14:49 +0800

    Update and rename canvas.njk to canvas.py

--- templates/snippets/canvas.py
+++ templates/snippets/canvas.py
@@ -1,4 +1,4 @@
-﻿<div class="plate">
+<div class="plate">
 	<canvas id="fluid" height="128" width="128"></canvas>
 	<canvas id="canvas" width="1440" height="780"></canvas>
 	<div class="grain"></div>
